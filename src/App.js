6f7d9f5aca03e4b4c3702fcc280c7c30903f9c17
sanket shrestha <133840007+stha-sanket@users.noreply.github.com>
2024-10-21 22:24:04 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Projects';
+import Blog from './components/Blog/Blog';
+import Resume from './components/Resume/Resume';
+import {NavBar} from "./components/Navbar/Navbar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

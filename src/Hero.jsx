75cb003ff3f57b2de1462d483b133e8db9eedc07
sanket shrestha <133840007+stha-sanket@users.noreply.github.com>
2024-10-22 22:48:21 +0545

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,3 +3,16 @@ import { useTypewriter } from "react-simple-typewriter";
 import CV from "../../assets/SudeepFullel_cv.pdf";
 import Don from "../../assets/123.jpg";
 import Title from "../layouts/title";
+
+const HeroAbout = () => {
+    const [text] = useTypewriter({
+        words: ["Data Science Enthusiast.", 
+            "CS Bachelor Student.", 
+            "Graphics designer.", 
+            "Datacamp Moderator.", 
+            "Friendly Guy."],
+        loop: true,
+        typeSpeed: 70,
+        deleteSpeed: 30,
+        delaySpeed: 1000,
+    });

5892159083943f7dc378de3f262bd9cb5bd88b00
sanket shrestha <133840007+stha-sanket@users.noreply.github.com>
2024-10-23 23:48:35 +0545

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,4 +1,4 @@
-KOimport React from "react";
+import React from "react";
 import { useTypewriter } from "react-simple-typewriter";
 import CV from "../../assets/SudeepFullel_cv.pdf";
 import Don from "../../assets/123.jpg";
